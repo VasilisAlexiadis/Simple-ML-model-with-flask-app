71801e9d8cfe16db0b037059f547a604e531c4c5
VasilisAlexiadis <valeks4444@gmail.com>
2024-04-27 19:00:57 +0300

    next

--- app.py
+++ app.py
@@ -11,12 +11,20 @@ model = pickle.load(open('model.pkl', 'rb'))
 def home():
     return 'Welcome to the Flask App! Use the /predict endpoint to make predictions.'
 
-
-@app.route('/predict', methods=['POST'])
+@app.route('/predict')
 def predict():
-    data = request.get_json()
-    prediction = model.predict(pd.DataFrame([data]))
-    return jsonify({'prediction': list(prediction)})
+    sepal_length = request.args.get('sepal_length')
+    sepal_width = request.args.get('sepal_width')
+    petal_length = request.args.get('petal_length')
+    petal_width = request.args.get('petal_width')
+    prediction = model.predict([[float(sepal_length), float(sepal_width), float(petal_length), float(petal_width)]])
+    return jsonify({'prediction': str(list(prediction)[0])})
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     data = request.get_json()
+#     prediction = model.predict(pd.DataFrame([data]))
+#     return jsonify({'prediction': list(prediction)})
 
 if __name__ == '__main__':
     app.run(debug=True)
