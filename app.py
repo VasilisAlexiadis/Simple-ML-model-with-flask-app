1bf2e8a1b0e7790957e2f7bdbb3517002dbcdeb5
VasilisAlexiadis <valeks4444@gmail.com>
2024-04-27 10:30:29 +0300

    next

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, request, jsonify
 import pickle
-import pandas as pd
+
 
 app = Flask(__name__)
 
 # Load the model (ensure the model.pkl file exists or adjust accordingly)
 model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return 'Welcome to the Flask App! Use the /predict endpoint to make predictions.'
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
